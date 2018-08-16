df8eded937a931774f4c18f4b70f83d46ed87863
Nathan Prabhu <nprab428@yahoo.com>
2018-08-16 10:18:59 -0500

    refactor to use snoowrap api

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,66 @@
-var express = require('express');
-var router = express.Router();
-const fetch = require('node-fetch');
+let express = require('express');
+let router = express.Router();
+const snoowrap = require('snoowrap');
+const credentials = require('../etc/credentials.json');
 
-let runPython = new Promise( (resolve, reject) => 
-{
-	const { spawn } = require('child_process');
-	const py = spawn('python', ['script.py']);
+// Create a new snoowrap requester with OAuth credentials.
+const r = new snoowrap({
+  userAgent: credentials.user_agent,
+  clientId: credentials.client_id,
+  clientSecret: credentials.client_secret,
+  username: credentials.username,
+  password: credentials.password
+});
 
-	let stdout = "";
-	py.stdout.on('data', (d) => {
-		stdout += d;
-		console.log(stdout);
-	});
+let keywords = ['thunder', 'okc', 'oklahoma']
 
-	py.stdout.on('end', () => {
-		resolve(stdout);
-	});
-})
+function checkKeywords(postTitle){
+	for (let i=0; i<keywords.length; i++){
+		if (postTitle.indexOf(keywords[i]) >= 0){
+			return true;
+		}
+	}
+	return false;
+}
 
-router.get('/', (req, res)=>{
-	getData('http://localhost:3000/api/data')
-		.then((data) => res.render('index', {title: "r/nba - OKC Thunder", data: data}));
-	});
-
-function getData(urlString){
-	return new Promise(function (resolve, reject){
-		fetch(urlString)
-			.then(res => res.json())
-			.then(data => resolve(data));
+function getData() { 
+	return r.getSubreddit('nba').getHot({limit:1000})
+	.filter(post => checkKeywords(post.title.toLowerCase()))
+	.map(post => {
+		return {
+			title: post.title,
+		 	score: post.score, 
+			comments: post.num_comments, 
+			url: post.url,
+			permalink: `https://www.reddit.com${post.permalink}`,
+			time: getRecency(post.created_utc)}
 	});
 }
-   
-router.get('/api/data', (req, res) => {
-	runPython.then(function(fromRunpy) {
-        res.json(fromRunpy.toString());
-    });
-})
-    
+
+function getRecency(timestamp){
+	const timeNow = Math.round((new Date()).getTime()/1000);
+	const minutesThreshold = 3600; 
+	const hoursThreshold = 24 * minutesThreshold;
+
+	let timeDiff = timeNow - timestamp;
+
+	if (timeDiff < minutesThreshold){
+		let relativeMinutes = Math.floor(timeDiff/60);
+		return `${relativeMinutes} minute${relativeMinutes == 1 ? '' : 's'} ago`;
+	}
+	else if (timeDiff < hoursThreshold){
+		let relativeHours = Math.floor(timeDiff/minutesThreshold);
+		return `${relativeHours} hour${relativeHours == 1 ? '' : 's'} ago`;
+	}
+	else {
+		let relativeDays = Math.floor(timeDiff/hoursThreshold);
+		return `${relativeDays} day${relativeDays == 1 ? '' : 's'} ago`;
+	}
+}
+
+router.get('/', (req, res)=>{
+	getData()
+		.then((data) => res.render('index', {title: "r/nba - OKC Thunder", data: data}));
+	});
 
 module.exports = router;
