506ed59ae0962aa632c19459a4bc7e58bad58f3c
Nathan Prabhu <nprab428@yahoo.com>
2018-08-16 11:48:43 -0500

    add eslint

--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,65 @@
-let express = require('express');
-let router = express.Router();
-const snoowrap = require('snoowrap');
+const express = require('express');
+
+const router = express.Router();
+const Snoowrap = require('snoowrap');
 const credentials = require('../etc/credentials.json');
 
 // Create a new snoowrap requester with OAuth credentials.
-const r = new snoowrap({
+const r = new Snoowrap({
   userAgent: credentials.user_agent,
   clientId: credentials.client_id,
   clientSecret: credentials.client_secret,
   username: credentials.username,
-  password: credentials.password
+  password: credentials.password,
 });
 
-let keywords = ['thunder', 'okc', 'oklahoma']
+const keywords = ['thunder', 'okc', 'oklahoma'];
 
-function checkKeywords(postTitle){
-	for (let i=0; i<keywords.length; i++){
-		if (postTitle.indexOf(keywords[i]) >= 0){
-			return true;
-		}
-	}
-	return false;
+function checkKeywords(postTitle) {
+  for (let i = 0; i < keywords.length; i += 1) {
+    if (postTitle.indexOf(keywords[i]) >= 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function getData() { 
-	return r.getSubreddit('nba').getHot({limit:1000})
-	.filter(post => checkKeywords(post.title.toLowerCase()))
-	.map(post => {
-		return {
-			title: post.title,
-		 	score: post.score, 
-			comments: post.num_comments, 
-			url: post.url,
-			permalink: `https://www.reddit.com${post.permalink}`,
-			time: getRecency(post.created_utc)}
-	});
+function getRecency(timestamp) {
+  const timeNow = Math.round((new Date()).getTime() / 1000);
+  const minutesThreshold = 3600;
+  const hoursThreshold = 24 * minutesThreshold;
+
+  const timeDiff = timeNow - timestamp;
+
+  if (timeDiff < minutesThreshold) {
+    const relativeMinutes = Math.floor(timeDiff / 60);
+    return `${relativeMinutes} minute${relativeMinutes === 1 ? '' : 's'} ago`;
+  }
+  if (timeDiff < hoursThreshold) {
+    const relativeHours = Math.floor(timeDiff / minutesThreshold);
+    return `${relativeHours} hour${relativeHours === 1 ? '' : 's'} ago`;
+  }
+
+  const relativeDays = Math.floor(timeDiff / hoursThreshold);
+  return `${relativeDays} day${relativeDays === 1 ? '' : 's'} ago`;
 }
 
-function getRecency(timestamp){
-	const timeNow = Math.round((new Date()).getTime()/1000);
-	const minutesThreshold = 3600; 
-	const hoursThreshold = 24 * minutesThreshold;
-
-	let timeDiff = timeNow - timestamp;
-
-	if (timeDiff < minutesThreshold){
-		let relativeMinutes = Math.floor(timeDiff/60);
-		return `${relativeMinutes} minute${relativeMinutes == 1 ? '' : 's'} ago`;
-	}
-	else if (timeDiff < hoursThreshold){
-		let relativeHours = Math.floor(timeDiff/minutesThreshold);
-		return `${relativeHours} hour${relativeHours == 1 ? '' : 's'} ago`;
-	}
-	else {
-		let relativeDays = Math.floor(timeDiff/hoursThreshold);
-		return `${relativeDays} day${relativeDays == 1 ? '' : 's'} ago`;
-	}
+function getData() {
+  return r.getSubreddit('nba').getHot({ limit: 1000 })
+    .filter(post => checkKeywords(post.title.toLowerCase()))
+    .map(post => ({
+      title: post.title,
+      score: post.score,
+      comments: post.num_comments,
+      url: post.url,
+      permalink: `https://www.reddit.com${post.permalink}`,
+      time: getRecency(post.created_utc),
+    }));
 }
 
-router.get('/', (req, res)=>{
-	getData()
-		.then((data) => res.render('index', {title: "r/nba - OKC Thunder", data: data}));
-	});
+router.get('/', (req, res) => {
+  getData()
+    .then(data => res.render('index', { title: 'r/nba - OKC Thunder', data }));
+});
 
 module.exports = router;
